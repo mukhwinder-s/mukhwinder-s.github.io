ef6b9307cdd8399c2ebf329dafaecd08a2f941e2
MSR <96829843+MSAuthentic@users.noreply.github.com>
2022-04-05 08:56:00 +0200

    Add files via upload
    Update at v. 05/04/2022 :  Updated "About Me".

--- Script.js
+++ Script.js
@@ -19,7 +19,7 @@
 
 var count = 0;
 var arr =[];
-var arrrand= [];
+
 // let box_tesla = document.querySelector('#box-tesla');
 let cv = document.querySelector("#canvas");
  
@@ -32,23 +32,21 @@ function CreaFulmini(e){
   const canvas = document.querySelector('#canvas');
   if (canvas.getContext){
 
-  console.log(count);
   const ctx = setupCanvas(document.querySelector('#canvas'));
   
   if (count ==1){
-    drawLine(ctx,  getCenter(), e, 2 );
+
+    drawLine(ctx,  getCenter(), e, 4 );
   }else if (count>2){
     count =0;
     drawLine(ctx, getCenter(),e, 5 );
 
   }
-
-
   
   }  
   cv = document.getElementById("canvas");
   cv.addEventListener("mouseleave", function pulisci(){ const canvas = document.querySelector('#canvas'); const context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height); context.clear();});
+  context.clearRect(0, 0, canvas.width, canvas.height);});
 }     
 
 
@@ -58,7 +56,7 @@ function drawLine(ctx, begin, e ,width = 1) {
  
 
   ctx.strokeStyle ='white';
-  ctx.shadowColor ='blue';
+  ctx.shadowColor ='violet';
   ctx.shadowOffsetX=3;
   ctx.shadowOffsetY=2;
   ctx.shadowBlur=15;
@@ -67,34 +65,37 @@ function drawLine(ctx, begin, e ,width = 1) {
   }
 
   ctx.beginPath();
-  
 
   ctx.moveTo(...begin);
   
   
   arr.push(GetPosition(e));
- 
+
  for(var i = 0; i<arr.length; i++){
      
+  
  
   ctx.lineTo(...arr[i]); 
  
+ 
  }
 
- 
   ctx.stroke();
  
   
 }
 
-function Rand(e){ x= e.clientX; y = e.clientY; var dimX = Math.sqrt(x+y); var dimY= Math.sqrt(x*y ); return [dimX,dimY]}
+function Rand(e){ x= e.clientX; y = e.clientY; var dimX = (2*Math.PI*x)/180; var dimY=(2*Math.PI*y*y)/180; return [dimX,dimY]}
 function GetPosition(e) {x = e.clientX; y = e.clientY; return[x,y]}
- 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
 function getCenter(){
    
    x = (cv.offsetWidth/2);
   y = (cv.scrollHeight/2);
-  console.log(x,y)
    return [x,y];
 }
 
@@ -112,3 +113,98 @@ function setupCanvas(canvas) {
   return ctx;
 }
 //FINE BOX FULIMINI
+
+
+// Ricarica al click sulle emojis
+function reload(){window.location.reload(true); }
+
+// CAMBIA LE EMOJIS
+async function changeEmojis(){
+
+while(true){
+
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "💘";
+
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "↩";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "✨";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "🥰";
+
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "😭";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "🔥";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "🌹";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "😋";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "😇";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "♻";
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "👌";
+
+  await delay(3000);
+
+  document.getElementById("emojis").innerHTML = "🙏";
+  
+}
+ 
+
+}
+
+changeEmojis()
+
+//FINE CAMBIO EMOJIS
+
+// THE GAME
+let _gameCanvas_ = document.querySelector("#TheGame");
+_gameCanvas_.addEventListener("mousedown",StartGame);
+
+function StartGame(e){
+
+if (_gameCanvas_.getContext){
+
+  _gameCanvasSetted_ = setupCanvas(document.querySelector('#TheGame'));
+  _gameCanvasSetted_.font = "30px Comic Sans MS";
+  _gameCanvasSetted_.fillText("🚀",...getCenterLeft())
+  
+  document.body.onkeydown = function(e){
+
+    switch(e.key){
+
+      case 32: // BARRA SPAZIATRICE
+
+
+
+    }
+
+
+  }
+
+}
+
+
+}
+function getCenterLeft(){
+   
+  x = (_gameCanvas_.offsetWidth/2);
+ y = (_gameCanvas_.scrollHeight/2);
+  return [x,y];
+}
+//FINE THE GAME
